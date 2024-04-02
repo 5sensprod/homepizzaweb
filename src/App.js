77337c26482f0a1393c21d8de3218679d11af4a5
chris <5sensprod@gmail.com>
2024-04-02 22:33:51 +0200

    ajout background

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import React from 'react'
-import { ThemeProvider } from './contexts/ThemeContext'
+import { ThemeProvider, useTheme } from './contexts/ThemeContext'
 import ThemedApp from './components/ui/ThemedApp'
 import Card from './components/ui/Card'
 import Box from './components/ui/Box'
-import BorderFrame from './components/ui/BorderFrame' // Importez BorderFrame
+import BorderFrame from './components/ui/BorderFrame'
 
-function App() {
+function AppContainer() {
+  const { theme } = useTheme()
+  const containerClass = `centered-container ${theme}`
+
   return (
-    <ThemeProvider>
-      <div className="centered-container">
-        <ThemedApp />
-        <BorderFrame bottom left>
-          <Box
-            title="PANINIS"
-            subtitle="Fermier"
-            price="8,50 €"
-            isVisible={true}
-            isLineVisible={true}
-          >
-            <p>Crème fraîche, fromage et poulet nature</p>
-          </Box>
-        </BorderFrame>
+    <div className={containerClass}>
+      <ThemedApp />
+      <BorderFrame bottom left>
+        <Box
+          title="PANINIS"
+          subtitle="Fermier"
+          price="8,50 €"
+          isVisible={true}
+          isLineVisible={true}
+        >
+          <p>Crème fraîche, fromage et poulet nature</p>
+        </Box>
+      </BorderFrame>
+      <BorderFrame bottom left>
         <Card
           title="Exemple de Titre"
           description="Ceci est une description pour illustrer comment utiliser Card dans App.js."
         />
-      </div>
+      </BorderFrame>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <ThemeProvider>
+      <AppContainer />
     </ThemeProvider>
   )
 }
