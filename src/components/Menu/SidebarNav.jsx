7657512a36c4c1bd2d4a7ce8d04a11b91a6ef681
chris <5sensprod@gmail.com>
2024-04-05 19:26:58 +0200

    ajout icon sidebarmenu

--- src/components/Menu/SidebarNav.jsx
+++ src/components/Menu/SidebarNav.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { ReactComponent as PizzaIcon } from '../../assets/icons/pizza-icon.svg'
 import { ReactComponent as PaniniIcon } from '../../assets/icons/panini-icon.svg'
 import { ReactComponent as GratinIcon } from '../../assets/icons/gratin-icon.svg'
+import { ReactComponent as DessertIcon } from '../../assets/icons/dessert-icon.svg'
+import { ReactComponent as DrinkIcon } from '../../assets/icons/dessert-icon.svg'
+import { ReactComponent as ZapwishIcon } from '../../assets/icons/zapwish-icon.svg'
+import { ReactComponent as TexMexIcon } from '../../assets/icons/texmex-icon.svg'
+import { ReactComponent as MenuDinishIcon } from '../../assets/icons/menu-dinish-icon.svg'
 // ... Importez d'autres icônes de même manière
 
 const SidebarNav = () => {
@@ -12,13 +17,26 @@ const SidebarNav = () => {
           <PizzaIcon /> Pizzas
         </li>
         <li>
+          <MenuDinishIcon /> Menus
+        </li>
+        <li>
           <PaniniIcon /> Paninis
         </li>
         <li>
           <GratinIcon /> Gratins
         </li>
-        {/* Ajoutez d'autres éléments de menu ici */}
-        {/* Utilisez les noms de fichiers icones appropriés */}
+        <li>
+          <TexMexIcon /> TexMex
+        </li>
+        <li>
+          <ZapwishIcon /> Zapwish
+        </li>
+        <li>
+          <DessertIcon /> Desserts
+        </li>
+        <li>
+          <DrinkIcon /> Boissons
+        </li>
       </ul>
     </nav>
   )
