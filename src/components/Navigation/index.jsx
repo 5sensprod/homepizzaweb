7ef3db7cc3eab8421348b8a9a097f7cb04ec06cf
chris <5sensprod@gmail.com>
2024-04-07 15:08:16 +0200

    clean

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,8 +1,7 @@
-// src/components/Navigation/index.jsx
 import React, { useState } from 'react'
 import { ReactComponent as MenuIcon } from '../../assets/icons/menu_icon_hand_drawn-min.svg'
 import { ReactComponent as PizzaScooterIcon } from '../../assets/icons/pizza_scooter_icon_hand_drawn.svg'
-import Menu from '../Menu' // Assurez-vous que le chemin d'importation est correct
+import Menu from '../Menu'
 
 const Navigation = () => {
   const [isMenuOpen, setMenuOpen] = useState(false)
