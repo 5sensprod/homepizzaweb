6494b106cf75b067cc0081374435ce9206597c6f
chris <5sensprod@gmail.com>
2024-04-07 14:39:09 +0200

    fix menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
 const Menu = ({ isOpen, onClose }) => {
   const navigate = useNavigate()
   const location = useLocation()
+
+  useEffect(() => {
+    // Fonction pour gérer le clic en dehors du menu
+    const handleOutsideClick = (event) => {
+      // Obtenir l'élément DOM pour le menu
+      const menuElement = document.querySelector('.menu')
+
+      // Vérifier si le menu est ouvert et si le clic n'a pas eu lieu à l'intérieur du menu
+      if (isOpen && menuElement && !menuElement.contains(event.target)) {
+        onClose() // Appel de la fonction pour fermer le menu
+      }
+    }
+
+    // Ajout de l'écouteur d'événements lors de l'ouverture du menu
+    document.addEventListener('mousedown', handleOutsideClick)
+
+    // Nettoyage de l'écouteur d'événements lors de la fermeture du menu ou du démontage du composant
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick)
+    }
+  }, [isOpen, onClose]) // Dépendances à isOpen et onClose
 
   if (!isOpen) {
     return null
@@ -11,14 +32,13 @@ const Menu = ({ isOpen, onClose }) => {
 
   const handleNavigate = (menuType) => {
     navigate(`/menu/${menuType}`)
-    onClose()
+    onClose() // Ferme le menu après la navigation
   }
 
   const isActive = (menuType) => location.pathname.includes(`/menu/${menuType}`)
 
   return (
     <div className="menu">
-      <button onClick={onClose}>Fermer</button>
       <ul>
         {/* <li>Menus</li>
         <li>Pizzas</li> */}
