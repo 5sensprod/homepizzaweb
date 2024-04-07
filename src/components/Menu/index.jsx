d996a82c1ce2137294fab8028c50657a0791e61b
chris <5sensprod@gmail.com>
2024-04-07 14:35:48 +0200

    useNavigation

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,31 +1,43 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const Menu = ({ isOpen, onClose }) => {
   const navigate = useNavigate()
+  const location = useLocation()
 
   if (!isOpen) {
     return null
   }
 
-  // Fonction pour gérer la navigation
   const handleNavigate = (menuType) => {
     navigate(`/menu/${menuType}`)
     onClose()
   }
 
+  const isActive = (menuType) => location.pathname.includes(`/menu/${menuType}`)
+
   return (
     <div className="menu">
       <button onClick={onClose}>Fermer</button>
       <ul>
-        <li>Menus</li>
-        <li>Pizzas</li>
-        <li onClick={() => handleNavigate('paninis')}>Paninis</li>
-        <li onClick={() => handleNavigate('gratins')}>Gratins</li>
-        <li>TexMex</li>
+        {/* <li>Menus</li>
+        <li>Pizzas</li> */}
+        <li
+          className={isActive('paninis') ? 'active' : ''}
+          onClick={() => handleNavigate('paninis')}
+        >
+          Paninis
+        </li>
+        <li
+          className={isActive('gratins') ? 'active' : ''}
+          onClick={() => handleNavigate('gratins')}
+        >
+          Gratins
+        </li>
+        {/* <li>TexMex</li>
         <li>Zapwich</li>
         <li>Desserts</li>
-        <li>Boissons</li>
+        <li>Boissons</li> */}
       </ul>
     </div>
   )
